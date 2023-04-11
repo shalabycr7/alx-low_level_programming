7849c6e6b631c8e556692c72b5850c5fe791db4e
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-04-11 13:50:38 -0700

    Fixed some corner cases

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,16 @@
 char *_strdup(char *str)
 {
 	char *s;
+	unsigned int i;
 
 	if (str == NULL)
 		return (NULL);
-	s = malloc(sizeof(str));
+	s = malloc(sizeof(str) + 1);
 	if (s == NULL)
 		return (NULL);
-	s = str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s[i] = str[i];
+	}
 	return (s);
 }
