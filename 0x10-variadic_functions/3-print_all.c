33163cc0626805e364b75d3eefdd8a01e7d742f4
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-04-20 13:25:50 -0700

    Write a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arguments passed.
+ * Return: no value.
+ */
+void print_all(const char *const format, ...)
+{
+	va_list ptr;
+	const char args[] = "ifsc";
+	char *s;
+	unsigned int i, j, flag = 0;
+
+	va_start(ptr, format);
+	for (i = 0; format != NULL && format[i]; i++)
+	{
+		for (j = 0; args[j]; j++)
+		{
+			if (format[i] == args[j] && flag == 1)
+			{
+				printf(", ");
+				break;
+			}
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int));
+				flag = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int));
+				flag = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double));
+				flag = 1;
+				break;
+			case 's':
+				s = va_arg(ptr, char *);
+				flag = 1;
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+		}
+	}
+	printf("\n");
+	va_end(ptr);
+}
